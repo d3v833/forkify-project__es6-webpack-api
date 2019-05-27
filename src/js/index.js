0b09a6467db0d58f52422465dc2744021275594d
d3v833 <d3v833@gmail.com>
2019-05-27 21:11:57 +0800

    adding search controller + updating config files

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
-import Search from './models/Search';
+import Search from './models/Search'
 
+const state = {}
 
-const state = {};
+const controlSearch = async () => {
+    // Notes: 1. Get Query from view
+    const query = 'pizza'
 
-const cpntrolSearch = () => {
-  
+    if (query) {
+        // Notes: 2. New search object and add to state
+        state.search = new Search(query)
+
+        // Notes: 3. Prepare UI for results
+
+        // Notes: 4. Search for recipes
+        await state.search.getResults()
+
+        // Notes: 5. Render results on UI
+        console.log(state.search.result)
+    }
 }
 
-
-const search = new Search('pizza');
-console.log(search)
-search.getResults();
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault()
+    controlSearch()
+})
