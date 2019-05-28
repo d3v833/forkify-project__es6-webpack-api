467ef88e2cf226c18250db76a6068ccd937a9836
d3v833 <d3v833@gmail.com>
2019-05-28 19:37:25 +0800

    implementing search results pagnation & updating config files

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,42 @@
-import Search from './models/Search'
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-const state = {}
+const state = {};
 
 const controlSearch = async () => {
     // Notes: 1. Get Query from view
-    const query = 'pizza'
+    const query = searchView.getInput();
+    console.log(query);
 
     if (query) {
         // Notes: 2. New search object and add to state
-        state.search = new Search(query)
+        state.search = new Search(query);
 
         // Notes: 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // Notes: 4. Search for recipes
-        await state.search.getResults()
+        await state.search.getResults();
 
         // Notes: 5. Render results on UI
-        console.log(state.search.result)
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
-}
+};
 
-document.querySelector('.search').addEventListener('submit', e => {
-    e.preventDefault()
-    controlSearch()
-})
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,20 +1,25 @@
 import axios from 'axios';
 
 export default class Search {
-  constructor(query) {
-    this.query = query;
-  }
-
-  async getResults(query) {
-    const key = '7e0d3735138f82dde35b419a682147df';
-    try {
-      const res = await axios(
-        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
-      );
-      this.result = res.data.recipes;
-      console.log(this.result);
-    } catch (error) {
-      alert(error);
+    constructor(query) {
+        this.query = query;
     }
-  }
+    // Notes: 7d0ab11e01b22aa37becf32d40564b6b
+    // Notes: 7e0d3735138f82dde35b419a682147df
+    // `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+
+    async getResults(query) {
+        const key = '';
+        try {
+            const res = await axios(
+                `https://www.food2fork.com/api/search?key=${key}&q=${
+                    this.query
+                }`
+            );
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
 }
